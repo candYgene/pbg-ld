90afd4a8d9c1a863722e95e0bccfbee03b44cda2
arnikz <a.kuzniar@esciencecenter.nl>
2017-01-17 17:58:55 +0100

    Fix SQL query for TGRC: remove rows with empty phen

--- src/tgrc-uniprot_query.sql
+++ src/tgrc-uniprot_query.sql
@@ -3,6 +3,13 @@
 .header on
 .output tgrc-uniprot.tsv
 
-SELECT A.gene gene1, A.des des1, A.chr chr1, phen phen1, B.gene gene2, B.alt_gene alt_gene2, B.des des2, B.chr chr2 FROM tgrc A, uniprot B WHERE A.chr = B.chr AND upper(replace(A.gene, '-', '')) = upper(replace(B.gene, '-', ''))
-UNION
-SELECT A.gene gene1, A.des des1, A.chr chr1, phen phen1, B.gene, B.alt_gene alt_gene2, B.des des2, B.chr chr2 FROM tgrc A, uniprot B WHERE A.chr = B.chr AND upper(replace(A.gene, '-', '')) = upper(replace(B.alt_gene, '-', ''));
+SELECT *
+FROM (
+   SELECT A.gene gene1, A.des des1, A.chr chr1, phen phen1, B.gene gene2, B.alt_gene alt_gene2, B.des des2, B.chr chr2
+   FROM tgrc A, uniprot B
+   WHERE A.chr = B.chr AND upper(replace(A.gene, '-', '')) = upper(replace(B.gene, '-', ''))
+   UNION
+   SELECT A.gene gene1, A.des des1, A.chr chr1, phen phen1, B.gene, B.alt_gene alt_gene2, B.des des2, B.chr chr2
+   FROM tgrc A, uniprot B
+   WHERE A.chr = B.chr AND upper(replace(A.gene, '-', '')) = upper(replace(B.alt_gene, '-', ''))
+) WHERE phen1 IS NOT NULL;
