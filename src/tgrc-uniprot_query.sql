c19d6e6341052b6f9f224cb082ee5197cce6f61b
arnikz <a.kuzniar@esciencecenter.nl>
2017-01-17 11:59:53 +0100

    Add TGRC data on tomato mutants & scripts to integrate with UniProt/EnsEMBL genomic data.

---
+++ src/tgrc-uniprot_query.sql
@@ -0,0 +1,8 @@
+.mode list
+.sep \t
+.header on
+.output tgrc-uniprot.tsv
+
+SELECT A.gene gene1, A.des des1, A.chr chr1, phen phen1, B.gene gene2, B.alt_gene alt_gene2, B.des des2, B.chr chr2 FROM tgrc A, uniprot B WHERE A.chr = B.chr AND upper(replace(A.gene, '-', '')) = upper(replace(B.gene, '-', ''))
+UNION
+SELECT A.gene gene1, A.des des1, A.chr chr1, phen phen1, B.gene, B.alt_gene alt_gene2, B.des des2, B.chr chr2 FROM tgrc A, uniprot B WHERE A.chr = B.chr AND upper(replace(A.gene, '-', '')) = upper(replace(B.alt_gene, '-', ''));
