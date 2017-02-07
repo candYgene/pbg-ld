1651090109738c97be3b35c02a21b7f60a4b55e1
arnikz <a.kuzniar@esciencecenter.nl>
2017-02-07 16:35:15 +0100

    Minor fix

---
+++ src/get_onto.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Batch script to download required ontologies in *.rdf.gz files and
+# to write graph URIs in *.graph files for RDF bulk loading.
+#
+
+curl --stderr - -LH "Accept: application/rdf+xml" -o faldo.rdf "http://biohackathon.org/resource/faldo.rdf" \
+	&& echo "http://biohackathon.org/resource/faldo.rdf" > faldo.rdf.graph
+
+curl --stderr - -LH "Accept: application/rdf+xml" -o so.rdf "http://purl.obolibrary.org/obo/so.owl" \
+	&& echo "http://purl.obolibrary.org/obo/so.owl" > so.rdf.graph
+
+curl --stderr - -LH "Accept: application/rdf+xml" -o sio.rdf "http://semanticscience.org/ontology/sio.owl" \
+	&& echo "http://semanticscience.org/ontology/sio.owl" > sio.rdf.graph
+
+curl --stderr - -LH "Accept: application/rdf+xml" -o ro.rdf "http://purl.obolibrary.org/obo/ro.owl" \
+	&& echo "http://purl.obolibrary.org/obo/ro.owl" > ro.rdf.graph
+
+gzip -9 *.rdf
