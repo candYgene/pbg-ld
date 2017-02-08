1b647e1155654b5c69b51656d9634dfe805aa9e2
arnikz <a.kuzniar@esciencecenter.nl>
2017-02-08 15:57:54 +0100

    Fix SGN graph URI; add variable for Ensembl rel.; remove miRNA type for protein-coding transcripts

--- src/update_rdf.sql
+++ src/update_rdf.sql
@@ -3,7 +3,8 @@
 --
 
 SET u{ENSEMBL_GRAPH_URI} http://plants.ensembl.org/Solanum_lycopersicum ;
-SET u{SGN_GRAPH_URI} https://solgenomics.net/genome/Solanum_lycopersicum ;
+SET u{ENSEMBL_RELEASE} 33 ;
+SET u{SGN_GRAPH_URI} http://solgenomics.net/genome/Solanum_lycopersicum ;
 
 SPARQL
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -29,16 +30,15 @@ WITH <$u{ENSEMBL_GRAPH_URI}>
 DELETE { ?s rdfs:seeAlso ?o }
 INSERT { ?s rdfs:seeAlso ?fixed }
 WHERE {
-   ?s a obo:SO_0000120 .
    ?s rdfs:seeAlso ?o .
    FILTER regex(?o, 'http://identifiers.org/ensembl') .
    BIND(uri(replace(str(?o), 'ensembl', 'ensembl.plant')) AS ?fixed)
 } ;
 
 
--- 
--- Add chromosome type to Ensembl RDF graph.
---
+----
+---- Add chromosome type to Ensembl RDF graph.
+----
 
 SPARQL
 PREFIX obo: <http://purl.obolibrary.org/obo/>
@@ -48,27 +48,27 @@ INSERT INTO <$u{ENSEMBL_GRAPH_URI}> {
 WHERE {
    GRAPH <$u{SGN_GRAPH_URI}> {
       ?chr1 a obo:SO_0000340 .
-      BIND(uri(concat('http://rdf.ebi.ac.uk/resource/ensembl/32/solanum_lycopersicum/SL2.50/', replace(str(?chr1), '.+ch0?', ''))) AS ?chr2)
+      BIND(uri(concat('http://rdf.ebi.ac.uk/resource/ensembl/$u{ENSEMBL_RELEASE}/solanum_lycopersicum/SL2.50/', replace(str(?chr1), '.+ch0?', ''))) AS ?chr2)
    }
 } ;
 
---
--- Delete triples
---
--- SPARQL
--- PREFIX obo: <http://purl.obolibrary.org/obo/>
--- WITH  <$u{ENSEMBL_GRAPH_URI}>
--- DELETE WHERE { ?s a obo:SO_0000340 } ;
+----
+---- Delete triples
+----
+---- SPARQL
+---- PREFIX obo: <http://purl.obolibrary.org/obo/>
+---- WITH  <$u{ENSEMBL_GRAPH_URI}>
+---- DELETE WHERE { ?s a obo:SO_0000340 } ;
 
 
--- 
--- Cross-link chromosomes in two RDF graphs.
---   graph URI: https://solgenomics.net/genome/Solanum_lycopersicum
---     chromosome URI: e.g. http://solgenomics.net/genome/Solanum_lycopersicum/chromosome/SL2.50ch01
---
---   graph URI: http://plants.ensembl.org/Solanum_lycopersicum
---     chromosome URI: e.g. http://rdf.ebi.ac.uk/resource/ensembl/32/solanum_lycopersicum/SL2.50/1
---
+----
+---- Cross-link chromosomes in two RDF graphs.
+----   graph URI: http://solgenomics.net/genome/Solanum_lycopersicum
+----     chromosome URI: e.g. http://solgenomics.net/genome/Solanum_lycopersicum/chromosome/SL2.50ch01
+----
+----   graph URI: http://plants.ensembl.org/Solanum_lycopersicum
+----     chromosome URI: e.g. http://rdf.ebi.ac.uk/resource/ensembl/33/solanum_lycopersicum/SL2.50/1
+----
 
 SPARQL
 PREFIX obo: <http://purl.obolibrary.org/obo/>
@@ -79,25 +79,25 @@ INSERT INTO <$u{SGN_GRAPH_URI}> {
 WHERE {
    GRAPH <$u{SGN_GRAPH_URI}> {
       ?chr1 a obo:SO_0000340 .
-      BIND(uri(concat('http://rdf.ebi.ac.uk/resource/ensembl/32/solanum_lycopersicum/SL2.50/', replace(str(?chr1), '.+ch0?', ''))) AS ?chr2)
+      BIND(uri(concat('http://rdf.ebi.ac.uk/resource/ensembl/$u{ENSEMBL_RELEASE}/solanum_lycopersicum/SL2.50/', replace(str(?chr1), '.+ch0?', ''))) AS ?chr2)
    }
 } ;
 
---
--- Delete triples
---
--- SPARQL
--- PREFIX owl: <http://www.w3.org/2002/07/owl#>
--- WITH  <$u{SGN_GRAPH_URI}>
--- DELETE WHERE {
---    ?s owl:sameAs ?o .
---    ?s a obo:SO_0000340
--- } ;
+----
+---- Delete triples
+----
+---- SPARQL
+---- PREFIX owl: <http://www.w3.org/2002/07/owl#>
+---- WITH  <$u{SGN_GRAPH_URI}>
+---- DELETE WHERE {
+----    ?s owl:sameAs ?o .
+----    ?s a obo:SO_0000340
+---- } ;
 
 
---
--- Link chromosomes to ENA accessions.
---
+----
+---- Link chromosomes to ENA accessions.
+----
 
 SPARQL
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -118,14 +118,14 @@ INSERT INTO <$u{SGN_GRAPH_URI}> {
    sgn-chr:SL2.50ch12 rdfs:seeAlso ena:CM001075.2
 } ;
 
---
--- Cross-link protein-coding genes in two RDF graphs.
---   graph URI: https://solgenomics.net/genome/Solanum_lycopersicum
---     gene URI: e.g. http://solgenomics.net/genome/Solanum_lycopersicum/gene/Solyc07g049530.2
---
---   graph URI: http://plants.ensembl.org/Solanum_lycopersicum
---     gene URI: e.g. http://rdf.ebi.ac.uk/resource/ensembl/Solyc07g049530.2
---
+----
+---- Cross-link protein-coding genes in two RDF graphs.
+----   graph URI: http://solgenomics.net/genome/Solanum_lycopersicum
+----     gene URI: e.g. http://solgenomics.net/genome/Solanum_lycopersicum/gene/Solyc07g049530.2
+----
+----   graph URI: http://plants.ensembl.org/Solanum_lycopersicum
+----     gene URI: e.g. http://rdf.ebi.ac.uk/resource/ensembl/Solyc07g049530.2
+----
 
 SPARQL
 PREFIX obo: <http://purl.obolibrary.org/obo/>
@@ -135,7 +135,7 @@ INSERT INTO <$u{SGN_GRAPH_URI}> {
 }
 WHERE {  
    GRAPH <$u{SGN_GRAPH_URI}> {
-      ?gene1 a obo:SO_0000704 .
+      ?gene1 a obo:SO_0001217 .
       BIND(uri(concat('http://rdf.ebi.ac.uk/resource/ensembl/', replace(str(?gene1), '.+/', ''))) AS ?gene2)
    }
    GRAPH <$u{ENSEMBL_GRAPH_URI}> {
@@ -144,9 +144,9 @@ WHERE {
 } ;
 
 
--- 
--- Add primary trascript type to Ensembl RDF graph.
---
+----
+---- Add primary trascript type to Ensembl RDF graph.
+----
 
 SPARQL
 PREFIX obo: <http://purl.obolibrary.org/obo/>
@@ -160,23 +160,32 @@ WHERE {
    }
 } ;
 
---
--- Delete triples
---
--- SPARQL
--- PREFIX obo: <http://purl.obolibrary.org/obo/>
--- WITH  <$u{ENSEMBL_GRAPH_URI}>
--- DELETE WHERE { ?s a obo:SO_0000120 } ;
+----
+---- Delete triples
+----
+---- SPARQL
+---- PREFIX obo: <http://purl.obolibrary.org/obo/>
+---- WITH  <$u{ENSEMBL_GRAPH_URI}>
+---- DELETE WHERE { ?s a obo:SO_0000120 } ;
 
+---
+--- Delete miRNA type for protein-coding primary transcripts.
+---
 
--- 
--- Cross-link transcripts in two RDF graphs.
---   graph URI: https://solgenomics.net/genome/Solanum_lycopersicum
---     transcript URI: e.g. http://solgenomics.net/genome/Solanum_lycopersicum/prim_transcript/Solyc02g079500.2.1
---
---   graph URI: http://plants.ensembl.org/Solanum_lycopersicum
---     transcript URI: e.g. http://rdf.ebi.ac.uk/resource/ensembl.transcript/Solyc02g079500.2.1
---
+SPARQL
+PREFIX obo: <http://purl.obolibrary.org/obo/>
+WITH  <$u{ENSEMBL_GRAPH_URI}>
+DELETE { ?s a obo:SO_0000276 }
+WHERE { ?s a obo:SO_0000120 } ;
+
+----
+---- Cross-link transcripts in two RDF graphs.
+----   graph URI: http://solgenomics.net/genome/Solanum_lycopersicum
+----     transcript URI: e.g. http://solgenomics.net/genome/Solanum_lycopersicum/prim_transcript/Solyc02g079500.2.1
+----
+----   graph URI: http://plants.ensembl.org/Solanum_lycopersicum
+----     transcript URI: e.g. http://rdf.ebi.ac.uk/resource/ensembl.transcript/Solyc02g079500.2.1
+----
 
 SPARQL
 PREFIX obo: <http://purl.obolibrary.org/obo/>
@@ -191,13 +200,13 @@ WHERE {
    }
 } ;
 
---
--- Delete triples
---
--- SPARQL
--- PREFIX owl: <http://www.w3.org/2002/07/owl#>
--- WITH  <$u{SGN_GRAPH_URI}>
--- DELETE WHERE {
---    ?s owl:sameAs ?o .
---    ?s a obo:SO_0000120
--- } ;
+----
+---- Delete triples
+----
+---- SPARQL
+---- PREFIX owl: <http://www.w3.org/2002/07/owl#>
+---- WITH  <$u{SGN_GRAPH_URI}>
+---- DELETE WHERE {
+----    ?s owl:sameAs ?o .
+----    ?s a obo:SO_0000120
+---- } ;
