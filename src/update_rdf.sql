d5fac3a2952ae992c3b8091385662e98b81d45b2
arnikz <a.kuzniar@esciencecenter.nl>
2017-03-17 11:01:03 +0100

    FIX: SIO predicate URI #45

--- src/update_rdf.sql
+++ src/update_rdf.sql
@@ -21,11 +21,10 @@ WHERE {
 } ;
 
 SPARQL
-PREFIX sio: <http://semanticscience.org/resource/>
 WITH <$u{ENSEMBL_G_URI}>
-DELETE { ?s sio:SIO:000630 ?o }
-INSERT { ?s sio:SIO_000630 ?o }
-WHERE { ?s sio:SIO:000630 ?o } ;
+DELETE { ?s <http://semanticscience.org/resource/SIO:000630> ?o }
+INSERT { ?s <http://semanticscience.org/resource/SIO_000630> ?o }
+WHERE { ?s <http://semanticscience.org/resource/SIO:000630> ?o } ;
 
 SPARQL
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
