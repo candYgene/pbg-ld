d4b13ac922c97adebd16d7fce98e81f6864d7283
Arnold Kuzniar <a.kuzniar@esciencecenter.nl>
2019-09-03 21:20:44 +0200

    Remove dirs.

--- src/tmp/siga.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-PREFIX=$1
-echo "Processing $PREFIX ..."
-python SIGA.py rdf -c $PREFIX.ini $PREFIX.db 
-echo Done.
-
--- src/tmp/submit_jobs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for d in $(cat *.txt)
-do
-   xenon -vvv scheduler slurm --location local:// submit --inherit-env --max-run-time 30 --working-directory . --stderr siga.stderr-%j.log --stdout siga.stdout-%j.log siga.sh $d
-done
-
