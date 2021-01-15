f54d2af86c5e410ddb4d45083aea60be0214d31e
arnikz <a.kuzniar@esciencecenter.nl>
2021-01-15 17:29:53 +0100

    Update config & disable some pckgs.

--- src/install_pkgs.sql
+++ src/install_pkgs.sql
@@ -1,8 +1,7 @@
---
--- Batch installer for Virtuoso VAD packages
---
-
 SET u{PATH} /usr/local/virtuoso-opensource/share/virtuoso/vad/ ;
+--VAD_UNINSTALL('fct/1.13.91') ;
+--VAD_UNINSTALL('rdb2rdf/1.00.0003') ;
+--VAD_UNINSTALL('rdf_mappers/1.34.74') ;
 
 VAD_INSTALL('$u{PATH}/fct_dav.vad', 0) ;
 --VAD_INSTALL('$u{PATH}/rdb2rdf_dav.vad', 0) ;
